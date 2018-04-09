607796bff28fc6a51bec4df6540b371de59926a1
Colin Nico Schmid <mail@mala23.me>
2018-04-09 10:03:17 +0200

    hello express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('may node be with you')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello from express')
+})
+
+app.listen(3000, function() {
+  console.log('listening on port 3000')
+})
