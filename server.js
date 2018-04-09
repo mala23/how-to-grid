760e92d496344b1cc0a60026772a1deb92acf3c2
Colin Nico Schmid <mail@mala23.me>
2018-04-09 10:08:49 +0200

    add nodemon

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('hello from express')
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3000, function() {
